ba0a9e308844857a9df11aa380926456fce5bd50
Mihail <lepadatu.mihail1@gmail.com>
2025-10-06 22:05:38 +0200

    fix: Add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,6 +97,17 @@ export default async function Layout({
           strategy="afterInteractive"
         />
 
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NH6BS3G8');
+          `}
+        </Script>
+
         {/* Google Tag Manager with Consent Mode */}
         <Script id="google-consent">
           {`
@@ -130,6 +141,17 @@ export default async function Layout({
         </Script>
       </head>
       <body className="flex min-h-full flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NH6BS3G8"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {children}
         <Analytics />
       </body>
